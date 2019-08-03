7c55024ed2703c3550288c558d36bc28c3104a31
thanhns <thanhns@ttc-solutions.com>
2019-08-03 11:05:14 +0700

    add EVreflect Pod for object

---
+++ Product_Discovery/ProductListing/Models/ProductsDTO.swift
@@ -0,0 +1,12 @@
+//
+//  ProductsDTO.swift
+//  Product_Discovery
+//
+//  Created by Nguyễn Thành on 8/3/19.
+//  Copyright © 2019 TrungNguyen. All rights reserved.
+//
+
+import Foundation
+import EVReflection
+
+
